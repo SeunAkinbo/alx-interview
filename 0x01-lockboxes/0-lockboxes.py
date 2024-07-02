c0130f1c8ac11346003d74fb636fec496f1f53f9
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-02 17:11:35 +0000

    LockBoxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module 0-lockboxes.py"""
+
+def canUnlockAll(boxes):
+    """Function that creates a list to keep track of opened boxes
+
+    Args:
+        boxes (list): Liat of Integers
+    """
+    opened = [False] * len(boxes)
+    # The first box is always unlocked
+    opened[0] = True
+    # Create a stack to keep track of the boxes to be opened
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key < len(boxes) and not opened[key]:
+                opened[key] = True
+                stack.append(key)
+
+    return all(opened)
